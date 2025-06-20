a3f5411fab0ba066eb2dbcba76f8a7d0b248a7ca
Erin McLaughlin <erinmclaughlin219@gmail.com>
2025-06-20 09:10:30 -0400

    manage app secrets from aspire apphost

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,5 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.App>("app");
+var githubAccessToken = builder.AddParameter("github-access-token", secret: true);
+
+var steamApiKey =  builder.AddParameter("steam-api-key", secret: true);
+var steamUserId =  builder.AddParameter("steam-id");
+
+builder.AddProject<Projects.App>("app")
+    .WithEnvironment("GitHub:AccessToken", githubAccessToken)
+    .WithEnvironment("Steam:ApiKey", steamApiKey)
+    .WithEnvironment("Steam:SteamId", steamUserId);
 
 builder.Build().Run();
