607c2b1d711f1c518da8d3b1a756781ef4fb77e2
Erin McLaughlin <erinmclaughlin219@gmail.com>
2025-06-20 10:51:00 -0400

    update aspire version

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,5 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var customDomain = builder.AddParameter("custom-domain");
+var certificateName = builder.AddParameter("certificate-name");
+
 var githubAccessToken = builder.AddParameter("github-access-token", secret: true);
 
 var steamApiKey =  builder.AddParameter("steam-api-key", secret: true);
@@ -9,6 +12,13 @@ builder.AddProject<Projects.App>("app")
     .WithEnvironment("github-access-token", githubAccessToken)
     .WithEnvironment("steam-api-key", steamApiKey)
     .WithEnvironment("steam-id", steamUserId)
-    .WithExternalHttpEndpoints();
+    .WithExternalHttpEndpoints()
+    .PublishAsAzureContainerApp((_, app) =>
+    {
+#pragma warning disable ASPIREACADOMAINS001
+        // this feature is currently experimental
+        app.ConfigureCustomDomain(customDomain, certificateName);
+#pragma warning restore ASPIREACADOMAINS001
+    });
 
 builder.Build().Run();
